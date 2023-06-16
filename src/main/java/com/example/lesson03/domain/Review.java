b0ae91cc1b8c1d8dc2f10d90746a7725ed1b4910
songsaemi <songsaemiye@naver.com>
2023-06-16 18:40:14 +0900

    230616 3강 예제

---
+++ src/main/java/com/example/lesson03/domain/Review.java
@@ -0,0 +1,64 @@
+package com.example.lesson03.domain;
+
+import java.util.Date;
+
+public class Review {
+	private String id;
+	private String storeName;
+	private String menu;
+	private String username;
+	private double point;
+	private String review;
+	private Date createdAt;
+	private Date updatedAt;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public double getPoint() {
+		return point;
+	}
+	public void setPoint(double point) {
+		this.point = point;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
